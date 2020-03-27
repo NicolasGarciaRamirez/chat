143ea80fb3a2f4ec106cb8309fb267813d2dd175
mvallejom <47168791+mvallejom@users.noreply.github.com>
2020-03-27 01:02:17 -0500

    profile adelantado

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,15 +1,31 @@
 @extends('template.template')
 @section('content')
-<div class="grid-container">
-    <div class="boxMargin">
+   <div class="profile-container">
+   <div class"background-profile">
        <img class="background-img" src="/images/audience-1850119_1920.jpg">
     </div>
-    <h4>Tarek Ghanameh</h4>
-    <button class="btn-profile">Edit Profile</button>
-    <button class="btn-profile">Share Profile</button>
-    <button class="btn-profile">Preview Profile</button>
-            <a class="profile-link" id="profile" href="#">Profile</a>
-            <a class="profile-link" id="Channel" href="#">Channel</a>
-</div>
+    <img class="profile-img" src="images/apple-audience-blue-844928.jpg">
+    <div class="row">
+        <div class="profile-info">
+            <h4 class="profile-name">Tarek Ghanameh</h4>
+        </div>
+        <div class="btn-group">
+            <button class="btn-profile">Edit Profile</button>
+            <button class="btn-profile">Share Profile</button>
+            <button class="btn-profile">Preview Profile</button>
+        </div>
+       
+    </div>
+    <div class="profile-info">
+        <label class="info" id="info1">MIXING ENGINEER</label>
+        <label class="info" id="info2">CONTRIBUTOR</label>
+    </div>
+    <div class="profile-links">
+        <a class="profile-link" id="channel" href="#">Channel</a>
+        <a class="profile-link" id="profile" href="#">Profile</a>
+    </div>
+   
+
+</div>10:06 p. m. 26/03/2020
 @endsection
 
