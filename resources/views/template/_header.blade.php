6a5a0a4cea0be139acec63a6f5de77328513c63c
Norwin Guerreo <norwingcruz@gmail.com>
2020-03-31 00:47:59 -0500

    create template
    nav
    header

---
+++ resources/views/template/_header.blade.php
@@ -0,0 +1,31 @@
+<header class="c-header c-header-light c-header-fixed main-header">
+    <button class="c-header-toggler c-class-toggler d-lg-none" type="button" data-target="#sidebar" data-class="c-sidebar-show">
+        <i class="cil-hamburger-menu text-white"></i>
+    </button>
+    <a class="c-header-brand d-lg-none" href="#">
+        <img src="/images/logo-white.svg" width="118" height="46" alt="Logo">
+    </a>
+    <button class="c-header-toggler c-class-toggler mfs-3 d-md-down-none" type="button" data-target="#sidebar" data-class="c-sidebar-lg-show" responsive="true">
+        <i class="cil-hamburger-menu text-white"></i>
+    </button>
+    <ul class="c-header-nav d-md-down-none">
+        <li class="c-header-nav-item px-3"><a class="c-header-nav-link active" href="#">Stream</a></li>
+        <li class="c-header-nav-item px-3"><a class="c-header-nav-link" href="#">Profile</a></li>
+    </ul>
+    <ul class="c-header-nav mfs-auto">
+        <form action="/" class="header-form-search d-none d-md-block">
+            <div class="form-group m-0">
+                <div class="input-group">
+                    <input class="form-control" type="text" placeholder="Search for posts, hashtags, etc..." autocomplete="none">
+                    <div class="input-group-append">
+                        <span class="input-group-text">
+                            <i class="cil-search"></i>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </form>
+        <li class="c-header-nav-item pl-2"><a class="c-header-nav-link other-link become-contributor d-none d-md-block" href="#">Become A Contributor </a></li>
+        <li class="c-header-nav-item px-2"><a class="c-header-nav-link other-link login" href="#">Login</a></li>
+    </ul>
+</header>
--- resources/views/template/_nav.blade.php
+++ resources/views/template/_nav.blade.php
@@ -1,1 +1,83 @@
-@include('template._footer')
+<div class="c-sidebar c-sidebar-dark c-sidebar-fixed c-sidebar-lg-show main-nav" id="sidebar">
+    <div class="c-sidebar-brand d-md-down-none">
+        <img src="/images/logo-white.svg" class="logo" alt="Logo">
+    </div>
+    <ul class="c-sidebar-nav ps ps--active-y px-3">
+        <li class="c-sidebar-nav-title pb-0">Filter Stream</li>
+        <li class="c-sidebar-nav-title p-0 m-0">
+            <img src="/images/sharks-menu.svg" alt="">
+        </li>
+        <li class="c-sidebar-nav-title">Gender</li>
+        <li class="c-sidebar-nav-item">
+            <input type="checkbox" id="pop">
+            <label for="pop"><span>Pop</span></label>
+        </li>
+        <li class="c-sidebar-nav-item">
+            <input type="checkbox" id="rap_hip_jop">
+            <label for="rap_hip_jop"><span>Rap & Hip-Jop</span></label>
+        </li>
+        <li class="c-sidebar-nav-item">
+            <input type="checkbox" id="edm">
+            <label for="edm"><span>EDM</span></label>
+        </li>
+        <li class="c-sidebar-nav-item">
+            <input type="checkbox" id="rock_metal">
+            <label for="rock_metal"><span>Rock & Metal</span></label>
+        </li>
+        <li class="c-sidebar-nav-item">
+            <input type="checkbox" id="jazz_blues">
+            <label for="jazz_blues"><span>Jazz & Blues</span></label>
+        </li>
+        <li class="c-sidebar-nav-item">
+            <input type="checkbox" id="classical">
+            <label for="classical"><span>Classical</span></label>
+        </li>
+        <li class="c-sidebar-nav-item">
+            <input type="checkbox" id="funk">
+            <label for="funk"><span>Funk</span></label>
+        </li>
+        <li class="c-sidebar-nav-item">
+            <input type="checkbox" id="world">
+            <label for="world"><span>World</span></label>
+        </li>
+        <li class="c-sidebar-nav-title">Category</li>
+        <li class="c-sidebar-nav-item">
+            <input type="checkbox" id="production_engineering">
+            <label for="production_engineering"><span>Production & Engineering</span></label>
+        </li>
+        <li class="c-sidebar-nav-item">
+            <input type="checkbox" id="vlogs">
+            <label for="vlogs"><span>Vlogs</span></label>
+        </li>
+        <li class="c-sidebar-nav-item">
+            <input type="checkbox" id="instruments">
+            <label for="instruments"><span>Instruments</span></label>
+        </li>
+        <li class="c-sidebar-nav-item">
+            <input type="checkbox" id="podcasts">
+            <label for="podcasts"><span>Podcasts</span></label>
+        </li>
+        <li class="c-sidebar-nav-item">
+            <input type="checkbox" id="audio_clips">
+            <label for="audio_clips"><span>Audio Clips</span></label>
+        </li>
+        <li class="c-sidebar-nav-item p-0">
+            <div class="divider"></div>
+        </li>
+        <li class="c-sidebar-nav-title m-0 pb-0">
+            Following
+            <i class="fas fa-star float-sm-right"></i>
+        </li>
+        <li class="c-sidebar-nav-title mt-0 pt-0">
+            <img src="/images/sharks-menu.svg" alt="">
+        </li>
+        <li class="c-sidebar-nav-item">
+            SoundStorming
+            <span class="float-sm-right c-tertiary dot">•</span>
+        </li>
+        <li class="c-sidebar-nav-item p-0">
+            <div class="divider"></div>
+        </li>
+        @include('template._footer')
+    </ul>
+</div>
