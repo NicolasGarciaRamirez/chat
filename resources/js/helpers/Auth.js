f0a62b4937e2f1d2f100ea34c16d8bf1e5c34875
Norwin Guerreo <norwingcruz@gmail.com>
2020-05-24 12:44:24 -0500

    first test

--- resources/js/helpers/Auth.js
+++ resources/js/helpers/Auth.js
@@ -1,19 +1,15 @@
 export default {
-    setAuthUser(Boolean, avatar, username ) {
+    setAuthUser(username ) {
         localStorage.setItem("username", username , {expires: 1})
-        localStorage.setItem("avatar", avatar , {expires: 1})
-        localStorage.setItem("auth", Boolean , {expires: 1});
+        localStorage.setItem("auth", true , {expires: 1});
     },
     getAuthUser() {
         return localStorage.getItem("auth");
-    },
-    getUserAvatar(){
-        return localStorage.getItem("avatar")
     },
     getUserName(){
         return localStorage.getItem("username");
     },
     deleteAuthUser() {
-        localStorage.removeItem("auth","avatar","username");
+        localStorage.removeItem("auth", "username");
     }
 };
