dceed1970384453f6cc5a9e389433159fd6d3688
Norwin Guerreo <norwingcruz@gmail.com>
2020-07-28 21:03:27 -0500

    update only one video play
    run prod

--- resources/js/Views/Home/Views/Index.vue
+++ resources/js/Views/Home/Views/Index.vue
@@ -168,6 +168,7 @@
     import ModalLiveStream from "../../CommingSoon/Components/ModalLiveStream";
     import ModalMobile from "../../CommingSoon/Components/ModalMobile";
     import ModalAlert from "../Components/ModalAlert";
+
     export default {
         name: "Index",
         props:['posts'],
