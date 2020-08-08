1f2d6bf1e0e34fcb71372835dcc9940aa9cf483b
Norwin Guerreo <norwingcruz@gmail.com>
2020-08-07 20:21:27 -0500

    Update template.blade.php

--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content= "width=device-width, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Noisesharks</title>
     <meta name="theme-color" content="#0d0d0d">
