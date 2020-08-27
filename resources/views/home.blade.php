9cadcf841b9047c886a36b3e4f8a530554c37adf
Norwin Guerreo <norwingcruz@gmail.com>
2020-08-26 21:17:12 -0500

    run watch

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,3 @@
         <home :posts="{{ $posts }}" ref="home"/>
     </div>
 @endsection
-
-@push('scripts')
-    <script>
-        console.log($('.post-type-video').find('wave'))
-    </script>
-@endpush
