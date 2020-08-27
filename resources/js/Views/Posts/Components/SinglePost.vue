3004e7da66c6826ad7b2d17f7bb5932d84693ace
Norwin Guerreo <norwingcruz@gmail.com>
2020-08-26 20:47:40 -0500

    test wave
    run prod

--- resources/js/Views/Posts/Components/SinglePost.vue
+++ resources/js/Views/Posts/Components/SinglePost.vue
@@ -109,7 +109,7 @@
                 <div class="d-flex flex-column content img-fluid p-3" v-if="post.resource">
                     <img v-gallery :src="`${post.resource}`" alt="img-post" class="cursor-point" v-if="post.resource_type === 'image'" @click="storeView" />
                     <video :src="`${post.resource}`" controls controlsList="nodownload" preload="none" v-if="post.resource_type === 'video'" @click="storeView" />
-                    <div :id="`waveform${post.token}`" @click="storeView" v-if="post.resource_type === 'audio'"></div>
+                    <div :id="`waveform${post.token}`" class="wave-form-main" @click="storeView" v-if="post.resource_type === 'audio'"></div>
                     <div class="d-flex flex-row text-center justify-content-center" v-if="post.resource_type === 'audio'"   >
                         <img src="/images/iconsplayer/Backward10sec-grey.svg" alt="" class="cursor-pointer" :id="`backward`+post.token" @click="backward(audio)" height="30" >
                         <div :id="`play`+post.token"  @click="playAudio()" >
@@ -253,6 +253,7 @@
             this.getLike()
             this.getVote()
             this.getFollow()
+            $(`#post-${this.post.token} .wave-form-main`).html('')
             if(this.post.resource_type === 'audio'){
                 this.createAudioWave()
                 this.wavesurfer.on('finish', () => {
@@ -288,6 +289,13 @@
                 return this.post.description
             }
         },
+        updated() {
+            post:{
+               if(this.post.resource_type === 'audio'){
+                   this.createAudioWave()
+               }
+            }
+        },
         methods:{
             // methods show
             showModalSupport(){
@@ -311,7 +319,9 @@
             //end methods show
             //methods player
             createAudioWave(){
+                $(`#post-${this.post.token} .wave-form-main`).html('')
                 var wave = `#waveform${this.post.token}`
+
                 this.wavesurfer = WaveSurfer.create({
                     container: wave,
                     backend: 'MediaElement',
